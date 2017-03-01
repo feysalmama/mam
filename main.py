997eefa11dc20eb2e28e05de9e4b7006c1b03659
Feysal Mama <feysaloo0922@gmail.com>
2017-03-01 11:30:38 -0800

    the first commit

---
+++ main.py
@@ -0,0 +1,18 @@
+from bestPractice.mystaff import Name
+
+#############################################
+#  other importing way  the module          #
+############################################
+
+from bestPractice import  mystaff
+
+
+c1  = Name("feysal",22,92205)
+
+# c1.name = "Mama"
+# c1.age = 22
+
+c1.feysal(c1.name,c1.age)
+
+
+
---
+++ mystaff.py
@@ -0,0 +1,7 @@
+class Name(object):
+    def __init__(self,name,age,phone):
+        self.name = name
+        self.age = age
+        self.address = phone
+    def feysal(self,name,age):
+        print("may name is {} and i am {} years old".format(name,age))
